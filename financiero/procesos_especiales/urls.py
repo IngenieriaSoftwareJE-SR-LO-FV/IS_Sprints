61d3c4771140ba05486f0175884121a5f4f09ca4
Sofia Rodriguez <sbrodrig@espol.edu.ec>
2020-02-02 10:29:03 -0500

    nota de credito: editar [#168956411]

--- financiero/procesos_especiales/urls.py
+++ financiero/procesos_especiales/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', views.index, name='procesos_especiales_index'),
     path('cambio_participante', views.cambio_participante_index, name='procesos_especiales_cambio_participante'),
     path('devolucion', views.devolucion_nuevo, name='procesos_especiales_devolucion'),
+    path('devolucion_editar/<pk>', views.devolucion_editar, name='procesos_especiales_devolucion_editar'),
     path('cambio_evento', views.cambio_evento_index, name='procesos_especiales_cambio_evento'),
     
 ]
--- financiero/procesos_especiales/views.py
+++ financiero/procesos_especiales/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import NotaCreditoForm
 from .models import NotaCredito
 
@@ -19,6 +19,18 @@ def devolucion_nuevo(request):
 	else:
 		form = NotaCreditoForm()
 	return render(request, "procesos_especiales/procesos_especiales_nota_credito_nuevo.html", {"form":form})
+
+def devolucion_editar(request, pk):
+	if(request.method == 'POST'):
+		p = get_object_or_404(NotaCredito, pk=pk)
+		form = NotaCreditoForm(request.POST, instance=p)
+		if(form.is_valid()):
+			form.save()
+			return redirect("procesos_especiales_index")
+	else:
+		p = get_object_or_404(NotaCredito, pk=pk)
+		form = NotaCreditoForm(instance=p)
+	return render(request, "procesos_especiales/procesos_especiales_nota_credito_editar.html", {'form': form})
 
 def cambio_evento_index(request):
 	return render(request, "procesos_especiales/procesos_especiales_index.html")
